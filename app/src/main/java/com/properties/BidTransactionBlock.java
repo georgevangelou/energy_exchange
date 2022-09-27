10944fbf91c86054b0db73ecf61cc32c2f2ae817
Nick Roussos <rouni97@hotmail.gr>
2022-09-27 14:28:02 +0200

    Add constructor

--- app/src/main/java/com/properties/BidTransactionBlock.java
+++ app/src/main/java/com/properties/BidTransactionBlock.java
@@ -1,4 +1,23 @@
 package com.properties;
 
-public class BidTransactionBlock {
+
+public class BidTransactionBlock extends Block{
+    private String receiverAddress;
+    private String publicKeySender;
+    private String privateKeySender;
+    private int coinAmount;
+    private int requestedEnergyAmount;
+    private int requestedPricePerEnergyUnit;
+    private byte[] signature;
+
+    public BidTransactionBlock(String blockHash, String previousBlockHash, int hashDifficulty, long timestamp, int index, String receiverAddress, String publicKeySender, String privateKeySender, int coinAmount, int requestedEnergyAmount, int requestedPricePerEnergyUnit, byte[] signature) {
+        super(blockHash, previousBlockHash, hashDifficulty, timestamp, index);
+        this.receiverAddress = receiverAddress;
+        this.publicKeySender = publicKeySender;
+        this.privateKeySender = privateKeySender;
+        this.coinAmount = coinAmount;
+        this.requestedEnergyAmount = requestedEnergyAmount;
+        this.requestedPricePerEnergyUnit = requestedPricePerEnergyUnit;
+        this.signature = signature;
+    }
 }
