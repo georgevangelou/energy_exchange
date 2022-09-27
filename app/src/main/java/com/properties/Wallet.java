52aaa02dd27ed1da5d89d356a991fd3555da5545
Nick Roussos <rouni97@hotmail.gr>
2022-09-27 15:06:44 +0200

    Add constructor

--- app/src/main/java/com/properties/Wallet.java
+++ app/src/main/java/com/properties/Wallet.java
@@ -1,4 +1,14 @@
 package com.properties;
 
 public class Wallet {
+    private int coinBalance;
+    private String publicKey;
+
+    public Wallet(int coinBalance, String publicKey, String privateKey) {
+        this.coinBalance = coinBalance;
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    private String privateKey;
 }
