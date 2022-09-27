6378ab1a895188cd9b8754feea5b8d713029d054
Nick Roussos <rouni97@hotmail.gr>
2022-09-27 15:05:46 +0200

    Add constructor

--- app/src/main/java/com/properties/Activity.java
+++ app/src/main/java/com/properties/Activity.java
@@ -1,4 +1,13 @@
 package com.properties;
 
 public class Activity {
+    private String activityHash;
+    private String clientAddress;
+    private boolean toFrom_;
+
+    public Activity(String activityHash, String clientAddress, boolean toFrom_) {
+        this.activityHash = activityHash;
+        this.clientAddress = clientAddress;
+        this.toFrom_ = toFrom_;
+    }
 }
