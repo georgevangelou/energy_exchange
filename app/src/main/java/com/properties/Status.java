cf763f77c317af174b26727c8612f10b2ef7dfd5
Nick Roussos <rouni97@hotmail.gr>
2022-09-27 14:24:12 +0200

    Add constructor

--- app/src/main/java/com/properties/Status.java
+++ app/src/main/java/com/properties/Status.java
@@ -1,4 +1,9 @@
 package com.properties;
 
 public class Status {
+    private int totalHashDifficulty;
+
+    public Status(int totalHashDifficulty){
+        this.totalHashDifficulty = totalHashDifficulty;
+    }
 }
