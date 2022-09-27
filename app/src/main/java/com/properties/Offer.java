32fb9000b60f625c1e792c195da73e1377e773b1
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-27 13:57:28 +0200

    Created offer

--- app/src/main/java/com/properties/Offer.java
+++ app/src/main/java/com/properties/Offer.java
@@ -5,4 +5,14 @@ package com.properties;
  * Created on: 2022-09-27
  */
 public class Offer {
+    private int totalTransferableEnergyAmount = 0;
+    private int pricePerEnergyUnit = 0;
+    private int producerAddress = 0;
+
+    public Offer(int totalTransferableEnergyAmount, int pricePerEnergyUnit, int producerAddress) {
+        this.totalTransferableEnergyAmount = totalTransferableEnergyAmount;
+        this.pricePerEnergyUnit = pricePerEnergyUnit;
+        this.producerAddress = producerAddress;
+    }
+
 }
