5fcf1c7aa5dfd9b7f1d19eec5cd54d599b0513f8
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-27 14:35:18 +0200

    Created Producer

---
+++ app/src/main/java/com/agents/Producer.java
@@ -0,0 +1,21 @@
+package com.agents;
+
+import com.properties.*;
+
+/**
+ * @author Georgios Evangelou
+ * Created on: 2022-09-27
+ */
+public class Producer {
+    private Status status;
+    private Offer offer;
+    private Blockchain blockchain;
+    private RecentActivity recentActivity;
+    private Activity activity;
+    private Wallet wallet;
+    private Battery battery;
+
+    public Producer() {
+        
+    }
+}
