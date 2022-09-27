7e0b8ccd48f49a93c349f65593ad96d91b450d13
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-27 14:45:38 +0200

    Created Consumer

---
+++ app/src/main/java/com/agents/Consumer.java
@@ -0,0 +1,20 @@
+package com.agents;
+
+import com.properties.*;
+
+/**
+ * @author Georgios Evangelou
+ * Created on: 2022-09-27
+ */
+public class Consumer {
+    private Status status;
+    private BidTransactionBlock bidTransactionBlock;
+    private Blockchain blockchain;
+    private RecentActivity recentActivity;
+    private Activity activity;
+    private Wallet wallet;
+    private Battery battery;
+
+    public Consumer() {
+    }
+}
