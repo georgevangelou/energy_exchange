643bebd26eba65070a4a9dddc4d42bd4dbde00f7
Nick Roussos <rouni97@hotmail.gr>
2022-09-27 15:06:06 +0200

    Minor refactoring

--- app/src/main/java/com/properties/BidTransactionBlock.java
+++ app/src/main/java/com/properties/BidTransactionBlock.java
@@ -4,17 +4,17 @@ package com.properties;
 public class BidTransactionBlock extends Block{
     private String receiverAddress;
     private String publicKeySender;
-    private String privateKeySender;
+    private String publicKeyReceiver;
     private int coinAmount;
     private int requestedEnergyAmount;
     private int requestedPricePerEnergyUnit;
     private byte[] signature;
 
-    public BidTransactionBlock(String blockHash, String previousBlockHash, int hashDifficulty, long timestamp, int index, String receiverAddress, String publicKeySender, String privateKeySender, int coinAmount, int requestedEnergyAmount, int requestedPricePerEnergyUnit, byte[] signature) {
+    public BidTransactionBlock(String blockHash, String previousBlockHash, int hashDifficulty, long timestamp, int index, String receiverAddress, String publicKeySender, String publicKeyReceiver, int coinAmount, int requestedEnergyAmount, int requestedPricePerEnergyUnit, byte[] signature) {
         super(blockHash, previousBlockHash, hashDifficulty, timestamp, index);
         this.receiverAddress = receiverAddress;
         this.publicKeySender = publicKeySender;
-        this.privateKeySender = privateKeySender;
+        this.publicKeyReceiver = publicKeyReceiver;
         this.coinAmount = coinAmount;
         this.requestedEnergyAmount = requestedEnergyAmount;
         this.requestedPricePerEnergyUnit = requestedPricePerEnergyUnit;
