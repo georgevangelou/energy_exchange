2f2fbbb1da026e6e74894ffb1519d96d90563796
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-27 13:58:17 +0200

    Removed set/get, added constructor

--- app/src/main/java/com/properties/PayTransactionBlock.java
+++ app/src/main/java/com/properties/PayTransactionBlock.java
@@ -19,99 +19,18 @@ public class PayTransactionBlock {
     private byte[] signature;
     private int index;
 
-    public String getHash() {
-        return hash;
-    }
-
-    public void setHash(String hash) {
+    public PayTransactionBlock(String hash, String previousBlockIde, String receiverAddress, String publicKeySender, String publicKeyReceiver, int hashDifficult, int coinAmount, int energyAmountToEmit, long deadline, long timestamp, byte[] signature, int index) {
         this.hash = hash;
-    }
-
-    public String getPreviousBlockIde() {
-        return previousBlockIde;
-    }
-
-    public void setPreviousBlockIde(String previousBlockIde) {
         this.previousBlockIde = previousBlockIde;
-    }
-
-    public String getReceiverAddress() {
-        return receiverAddress;
-    }
-
-    public void setReceiverAddress(String receiverAddress) {
         this.receiverAddress = receiverAddress;
-    }
-
-    public String getPublicKeySender() {
-        return publicKeySender;
-    }
-
-    public void setPublicKeySender(String publicKeySender) {
         this.publicKeySender = publicKeySender;
-    }
-
-    public String getPublicKeyReceiver() {
-        return publicKeyReceiver;
-    }
-
-    public void setPublicKeyReceiver(String publicKeyReceiver) {
         this.publicKeyReceiver = publicKeyReceiver;
-    }
-
-    public int getHashDifficult() {
-        return hashDifficult;
-    }
-
-    public void setHashDifficult(int hashDifficult) {
         this.hashDifficult = hashDifficult;
-    }
-
-    public int getCoinAmount() {
-        return coinAmount;
-    }
-
-    public void setCoinAmount(int coinAmount) {
         this.coinAmount = coinAmount;
-    }
-
-    public int getEnergyAmountToEmit() {
-        return energyAmountToEmit;
-    }
-
-    public void setEnergyAmountToEmit(int energyAmountToEmit) {
         this.energyAmountToEmit = energyAmountToEmit;
-    }
-
-    public long getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(long deadline) {
         this.deadline = deadline;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
-    }
-
-    public byte[] getSignature() {
-        return signature;
-    }
-
-    public void setSignature(byte[] signature) {
         this.signature = signature;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
         this.index = index;
     }
 }
