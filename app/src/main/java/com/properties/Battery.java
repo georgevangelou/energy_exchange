93601d4b9fa1d1366b863de8b78d4879a39859a3
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-27 14:10:45 +0200

    Filled Battery

--- app/src/main/java/com/properties/Battery.java
+++ app/src/main/java/com/properties/Battery.java
@@ -1,4 +1,11 @@
 package com.properties;
 
 public class Battery {
+    private int energyCapacity;
+    private int currentEnergyCapacity;
+
+    public Battery(int energyCapacity, int currentEnergyCapacity) {
+        this.energyCapacity = energyCapacity;
+        this.currentEnergyCapacity = currentEnergyCapacity;
+    }
 }
