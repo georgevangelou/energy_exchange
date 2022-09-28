9c4e878fd7e2f288ac5b750a18273091ff15e40e
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-28 11:17:42 +0200

    Draft implementations for Producer

--- app/src/main/java/app/agents/Producer.java
+++ app/src/main/java/app/agents/Producer.java
@@ -2,8 +2,16 @@ package app.agents;
 
 import app.properties.*;
 
+import java.util.Random;
+
 
 public class Producer {
+    //Functionality
+    //o   Generate expected energy production
+    //o   Generate energy unit price required to make profit
+    //o   Update stored blockchain (i.e., replace chain)
+    //o   Submit energy offer to smart contract
+
     private Status status;
     private Offer offer;
     private Blockchain blockchain;
@@ -12,7 +20,39 @@ public class Producer {
     private Wallet wallet;
     private Battery battery;
 
-    public Producer() {
+    private Random random = new Random();
 
+    public Producer() {
+    }
+
+
+    private int generateExpectedEnergyProduction() {
+        int low = 5;
+        int high = 10;
+        int expectedEnergyProduction = random.nextInt(high - low) + low;
+        return expectedEnergyProduction;
+    }
+
+
+    private int generateEnergyUnitPrice() {
+        int low = 10;
+        int high = 100;
+        int energyUnitPrice = random.nextInt(high - low) + low;
+        return energyUnitPrice;
+    }
+
+    private synchronized void updateStoredBlockChain() {
+        Block receivedBlock = null;
+
+        if (receivedBlock.getHashDifficulty() > this.blockchain.peekLast().getHashDifficulty()) {
+            this.blockchain.removeLast();
+            this.blockchain.add(receivedBlock);
+        }
+    }
+
+    private void submitEnergyOfferToSmartContract() {
+        int expectedEnergyProduction = generateExpectedEnergyProduction();
+        int energyUnitPrice = generateEnergyUnitPrice();
+        // TODO: pack and send
     }
 }
