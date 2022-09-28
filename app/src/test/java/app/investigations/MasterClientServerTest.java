2ec2ba45644a7772e83d134bf668f71d24686977
Nick Roussos <rouni97@hotmail.gr>
2022-09-28 12:09:25 +0200

    Add test for serializing/deserializing JSON content

---
+++ app/src/test/java/app/investigations/MasterClientServerTest.java
@@ -0,0 +1,37 @@
+package app.investigations;
+
+import com.google.gson.JsonObject;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static app.investigations.MasterClientServer.createNodes;
+import static org.junit.Assert.*;
+
+public class MasterClientServerTest {
+
+    @Test
+    public void jsonSerializeDeserialize() throws InterruptedException, IOException {
+        int count = 2;
+        ArrayList<PeerNode> arrayOfNodes = createNodes(count);
+        Thread.sleep(500);
+        System.out.println(arrayOfNodes.get(0).getPort());
+        System.out.println(arrayOfNodes.get(1).getPort());
+
+        JsonObject json;
+        for (int i = 0; i < 10; i++) {
+            json = new JsonObject();
+            json.addProperty("type", "r01");
+            json.addProperty("index", i++);
+            arrayOfNodes.get(0).sendRequest("localhost", arrayOfNodes.get(1).getPort(), json);
+            Thread.sleep(500);
+
+            json = new JsonObject();
+            json.addProperty("type", "r01");
+            json.addProperty("index", i++);
+            arrayOfNodes.get(1).sendRequest("localhost", arrayOfNodes.get(0).getPort(), json);
+            Thread.sleep(500);
+        }
+    }
+}
