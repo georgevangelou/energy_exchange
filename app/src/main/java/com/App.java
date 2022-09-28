73fc54e91ffdc9a4499eaa8a90a19eca3d479316
Nick Roussos <rouni97@hotmail.gr>
2022-09-28 09:47:20 +0200

    Remove unused imports

--- app/src/main/java/com/App.java
+++ app/src/main/java/com/App.java
@@ -3,10 +3,6 @@
  */
 package com;
 
-import org.eclipse.leshan.client.californium.LeshanClient;
-import org.eclipse.leshan.client.californium.LeshanClientBuilder;
-import org.eclipse.leshan.server.californium.LeshanServer;
-import org.eclipse.leshan.server.californium.LeshanServerBuilder;
 
 public class App {
     public String getGreeting() {
