a7800b8b3caa035048e377e87c87fe18de390052
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-28 14:48:17 +0200

    Added 'extends Block' where needed

--- app/src/main/java/app/properties/PayTransactionBlock.java
+++ app/src/main/java/app/properties/PayTransactionBlock.java
@@ -1,33 +1,24 @@
 package app.properties;
 
 
-public class PayTransactionBlock {
-    private String hash;
-    private String previousBlockIde;
+public class PayTransactionBlock extends Block {
     private String receiverAddress;
     private String publicKeySender;
     private String publicKeyReceiver;
-    private int hashDifficult;
     private int coinAmount;
     private int energyAmountToEmit;
 
     private long deadline;
-    private long timestamp;
     private byte[] signature;
-    private int index;
 
-    public PayTransactionBlock(String hash, String previousBlockIde, String receiverAddress, String publicKeySender, String publicKeyReceiver, int hashDifficult, int coinAmount, int energyAmountToEmit, long deadline, long timestamp, byte[] signature, int index) {
-        this.hash = hash;
-        this.previousBlockIde = previousBlockIde;
+    public PayTransactionBlock(String previousBlockHash, String receiverAddress, String publicKeySender, String publicKeyReceiver, int hashDifficulty, int coinAmount, int energyAmountToEmit, long deadline, long timestamp, byte[] signature, int index) {
+        super(previousBlockHash, hashDifficulty, timestamp, index);
         this.receiverAddress = receiverAddress;
         this.publicKeySender = publicKeySender;
         this.publicKeyReceiver = publicKeyReceiver;
-        this.hashDifficult = hashDifficult;
         this.coinAmount = coinAmount;
         this.energyAmountToEmit = energyAmountToEmit;
         this.deadline = deadline;
-        this.timestamp = timestamp;
         this.signature = signature;
-        this.index = index;
     }
 }
--- app/src/main/java/app/properties/RefundTransactionBlock.java
+++ app/src/main/java/app/properties/RefundTransactionBlock.java
@@ -1,30 +1,20 @@
 package app.properties;
 
 
-public class RefundTransactionBlock {
-    //Refund transaction block: LWM2M object
-    private String hash;
-    private String previousBlockHash;
+public class RefundTransactionBlock extends Block {
     private String receiverAddress;
     private String publicKeySender;
     private String publicKeyReceiver;
-    private int hashDifficulty;
     private int coinAmount;
     private byte[] signature;
-    private long timestamp;
-    private int index;
 
-    public RefundTransactionBlock(String hash, String previousBlockHash, String receiverAddress, String publicKeySender, String publicKeyReceiver, int hashDifficulty, int coinAmount, byte[] signature, long timestamp, int index) {
-        this.hash = hash;
-        this.previousBlockHash = previousBlockHash;
+    public RefundTransactionBlock(String previousBlockHash, String receiverAddress, String publicKeySender, String publicKeyReceiver, int hashDifficulty, int coinAmount, byte[] signature, long timestamp, int index) {
+        super(previousBlockHash, hashDifficulty, timestamp, index);
         this.receiverAddress = receiverAddress;
         this.publicKeySender = publicKeySender;
         this.publicKeyReceiver = publicKeyReceiver;
-        this.hashDifficulty = hashDifficulty;
         this.coinAmount = coinAmount;
         this.signature = signature;
-        this.timestamp = timestamp;
-        this.index = index;
     }
 }
 
