3665df17bc243be5d36a48ceba473f255c9d25cf
Nick Roussos <rouni97@hotmail.gr>
2022-09-28 12:09:45 +0200

    Move main to test

--- app/src/main/java/app/investigations/MasterClientServer.java
+++ app/src/main/java/app/investigations/MasterClientServer.java
@@ -1,26 +1,12 @@
 package app.investigations;
 
+import com.google.gson.JsonObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MasterClientServer {
     private final static int port = 0;
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        int count = 2;
-        ArrayList<PeerNode> arrayOfNodes = createNodes(count);
-        Thread.sleep(500);
-        System.out.println(arrayOfNodes.get(0).getPort());
-        System.out.println(arrayOfNodes.get(1).getPort());
-
-        int i = 0;
-        while (true) {
-            arrayOfNodes.get(0).sendRequest("localhost", arrayOfNodes.get(1).getPort(), Integer.toString(i++));
-            Thread.sleep(500);
-            arrayOfNodes.get(1).sendRequest("localhost", arrayOfNodes.get(0).getPort(), Integer.toString(i++));
-            Thread.sleep(500);
-        }
-    }
 
     public static ArrayList<PeerNode> createNodes(int count) {
         System.out.println("Creating a network of " + count + " nodes...");
