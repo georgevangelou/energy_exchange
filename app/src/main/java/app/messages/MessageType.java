2ba6c69774f5cb42643fb460e65f5931cbe72946
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-29 12:03:18 +0200

    Added message types

---
+++ app/src/main/java/app/messages/MessageType.java
@@ -0,0 +1,13 @@
+package app.messages;
+
+public class MessageType {
+    public static final String STATUS = "R01";
+    public static final String BLOCKCHAIN = "R02";
+    public static final String CURRENT_STATE = "R03";
+    public static final String RECEIVED_BLOCK = "W01";
+    public static final String OFFER = "W02";
+    public static final String WALLET = "W03";
+    public static final String PAY_BLOCK = "W04";
+    public static final String REFUND_BLOCK = "W05";
+    public static final String COIN_AMOUNT = "A01";
+}
