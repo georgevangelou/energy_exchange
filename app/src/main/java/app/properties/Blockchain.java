09c8e667bfc647df25cbf9fd45cd8aeb28fd4af0
Georgios Evangelou <gevangelou@hotmail.com>
2022-09-29 12:03:07 +0200

    Blockchain validity check

--- app/src/main/java/app/properties/Blockchain.java
+++ app/src/main/java/app/properties/Blockchain.java
@@ -1,10 +1,23 @@
 package app.properties;
 
+import app.utilities.StringUtil;
+
 import java.util.LinkedList;
 
 public class Blockchain extends LinkedList<Block> {
-    public boolean isValid() {
-        return true; //TODO: Implement
-    }
 
+    public boolean isValid() {
+        for (Block block : this) {
+            String calculatedHash =
+                    StringUtil.applySha256(
+                            block.getPreviousBlockHash()
+                                    + Long.toString(block.getTimestamp())
+                                    + Integer.toString(block.getIndex())
+                    );
+            if (!block.getHash().equals(calculatedHash)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
